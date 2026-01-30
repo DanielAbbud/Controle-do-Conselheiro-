1e2c366d7dbe1e35b123d620a552cc2ba528d2bb
DanielAbbud <danielabbud2003@gmail.com>
2026-01-30 08:28:34 -0300

    versão com PWA

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "dbv-app-v1";
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./logo.png",
+    "./manifest.json",
+    "./politica.html",
+    "./termos.html"
+];
+
+// Instala o App no Cache do Celular
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("Arquivos em cache");
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Busca do Cache quando estiver offline ou carregando
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
